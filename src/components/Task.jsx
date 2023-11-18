db1205e0162f6d017da543150c6611c379c66c33
JhonFDev <jhonfdeveloper@gmail.com>
2023-11-17 21:33:14 -0500

    colaboracion con camilo monsalve

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,63 @@
+import { useState } from "react";
+import TaskItem from "./TaskItem";
 
-const Tasks = [
-  { title: "comprar leche", completed: true, id: 1 },
-  { title: "sacar a los perros", completed: true, id: 2 },
-  { title: "recolectar Manzanas", completed: false, id: 3 },
-  { title: "sembrar Pimenton", completed: false, id: 4 },
-  { title: "sembrar tomate", completed: false, id: 5 },
-  { title: "Lavar marranos", completed: false, id: 6 },
-];
+function Tasks() {
+  const [tasks, setTasks] = useState();
+  const [newTask, setNewTask] = useState("");
+
+  fetch("http://localhost:5000/tasks", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setTasks(data)
+    })
+    .catch((err) => {
+      console.error("Ocurrio un error:" + err);
+    });
+
+  const handleAddTask = () => {
+    if (newTask) {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const handleDeleteTask = (task) => {
+    const updatedTasks = tasks.filter((t) => t !== task);
+    setTasks(updatedTasks);
+  };
+
+  const handleUpdateTask = (task) => {
+    const updatedTasks = tasks.map((t) => (t === task ? newTask : t));
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="tasks">
+      <ul>
+        {tasks.map((task, index) => (
+          <TaskItem
+            key={index}
+            task={task}
+            onDelete={handleDeleteTask}
+            onUpdate={handleUpdateTask}
+          />
+        ))}
+      </ul>
+      <input
+        type="text"
+        placeholder="Nueva tarea"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button onClick={handleAddTask}>Agregar tarea</button>
+    </div>
+  );
+}
 
 export default Tasks;
