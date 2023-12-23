b7fbafcb0044e425d292ce216a9ebd44add86720
JhonFDev <jhonfdeveloper@gmail.com>
2023-12-22 23:50:39 -0500

    se añadio diseño, Completa la lista de tareas y se Implement el modo oscuro y claro

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 // import "./TaskItemStyle.css"
 import PropTypes from "prop-types";
-import { Checkbox, Text, Box} from "@chakra-ui/react";
-import { mdiCheckDecagram, mdiTrashCanOutline, mdiNoteEditOutline } from "@mdi/js";
+import {
+  Checkbox,
+  Text,
+  Box,
+  UnorderedList,
+  List,
+  Stack,
+  useColorMode,
+  Input,
+  IconButton,
+} from "@chakra-ui/react";
+import {
+  mdiCheckDecagram,
+  mdiTrashCanOutline,
+  mdiNoteEditOutline,
+} from "@mdi/js";
 import Icon from "@mdi/react";
-
 
 function TaskItem({ task, onDelete, onUpdate }) {
   TaskItem.propTypes = {
@@ -16,52 +29,104 @@ function TaskItem({ task, onDelete, onUpdate }) {
     onUpdate: PropTypes.func.isRequired, // Ejemplo: Se espera que 'onUpdate' sea una función y sea requerida.
   };
 
-  const [isEditing, setEditing] = useState(false);
+  const [isEditing, setEditing] = useState(null);
   const [editedTask, setEditedTask] = useState(task);
+  const { colorMode } = useColorMode();
+  const isDarkMode = colorMode === "dark";
 
-  const handleEdit = () => {
+  const handleEdit = (title) => {
     setEditing(true);
+    setEditedTask(task[title]);
   };
 
   const handleSave = () => {
     onUpdate(editedTask);
-    setEditing(false);
+    setEditing(null);
   };
 
   return (
     <Box>
-        <div className="cardforlist">
-          <ul>
-          <Checkbox colorScheme="green" size="lg" bg="white" borderColor={"transparent"} borderStyle={"dashed"}  borderRadius={20} cursor={"pointer"}/>
+      <UnorderedList display="flex" flexDirection="row">
+        <List
+          display="flex"
+          w="400px"
+          justifyContent="space-between"
+          alignItems="center"
+          marginTop="10px"
+          bg={isDarkMode ? "#eeeeef80" : "#eeeeef"}
+          borderRadius={5}
+          marginRight={30}
+        >
+          <Stack>
+            <Checkbox
+              colorScheme="green"
+              size="lg"
+              bg="white"
+              borderColor={"transparent"}
+              left={5}
+              borderRadius={20}
+              cursor={"pointer"}
+            />
+          </Stack>
+          <Text w="200px" textColor={"black"}>
+            {task.title}
+          </Text>
+          {isEditing !== null ? (
+            <>
+              <Input
+                type="text"
+                value={editedTask}
+                onChange={(e) => setEditedTask(e.target.value)}
+              />
+              <IconButton
+                bg="none"
+                onClick={handleSave}
+                icon={
+                  colorMode === "light" ? (
+                    <Icon path={mdiCheckDecagram} size={1.2} color="#04caf1" />
+                  ) : (
+                    <Icon path={mdiCheckDecagram} size={1.2} color="white" />
+                  )
+                }
+              />
+            </>
+          ) : (
+            <>
+              <IconButton
+                bg="none"
+                onClick={handleEdit}
+                icon={
+                  colorMode === "light" ? (
+                    <Icon
+                      path={mdiNoteEditOutline}
+                      size={1.2}
+                      color="#04caf1"
+                    />
+                  ) : (
+                    <Icon path={mdiNoteEditOutline} size={1.2} color="#04caf1" />
+                  )
+                }
+              />
+            </>
 
-          <Text w="200px" >
-          {task.title}
-        </Text>
-            {isEditing ? (
-              <>
-                <input
-                  type="text"
-                  value={editedTask}
-                  onChange={(e) => setEditedTask(e.target.value)}
-                />
-                <button onClick={handleSave}>
-                <Icon path={mdiCheckDecagram} size={1.2} />
-                </button>
-              </>
-            ) : (
-              <>
-                <button onClick={handleEdit}>
-                <Icon path={mdiNoteEditOutline} size={1} color={"#646cff"} />
-                </button>
-              </>
-            )}
-            
-
-            <button onClick={() => onDelete(task)}>
-            <Icon path={mdiTrashCanOutline} size={1} color={"#e63739"} />
-            </button>
-          </ul>
-        </div>
+          )}
+          <IconButton
+                bg="none"
+                onClick={() => onDelete(task)}
+                icon={
+                  colorMode === "light" ? (
+                    <Icon
+                      path={mdiTrashCanOutline}
+                      size={1.2}
+                      color="#e63739"
+                    />
+                  ) : (
+                    <Icon path={mdiTrashCanOutline} size={1.2} color="#e63739" />
+                  )
+                }
+              />          
+        </List>
+      </UnorderedList>      
     </Box>
   );
 }
